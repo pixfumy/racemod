bc4de920733518148a772d973b4835c47c3f94f5
pixfumy <asaddurrani1@live.ca>
2022-08-22 20:30:02 -0400

    standardize cow, pig, and sheep drops

--- src/main/java/io/github/marinersfan824/racemod/RNGStreamGenerator.java
+++ src/main/java/io/github/marinersfan824/racemod/RNGStreamGenerator.java
@@ -16,7 +16,7 @@ public class RNGStreamGenerator {
 
     public void init(long worldSeed) {
         /*  abstracts some of the standardization into this HashMap. To add a new standardized source of RNG, put your entries
-            into this map, use your world's RNGStreamGenerator in a mixin, and then update tellPlayerCurrentRates.
+            into this map, use your world's RNGStreamGenerator in a mixin, and then update tellPlayerCurrentRates if you want to.
         */
         rngSeeds = new HashMap() {
             {
@@ -27,6 +27,9 @@ public class RNGStreamGenerator {
                 put("enderEyeSeed", worldSeed ^ 0x99A2B75BBL);
                 put("flintSeed", worldSeed ^ 0xF110301001B2L);
                 put("stringSeed", worldSeed ^ 0x120012034131L);
+                put("woolSeed", worldSeed ^ 0x7F12468038952L);
+                put("porkChopSeed", worldSeed ^ 64182641824614L);
+                put("beefSeed", worldSeed ^ 0xFF97FD1823L);
             }
         };
     }
@@ -57,93 +60,97 @@ public class RNGStreamGenerator {
         return rngSeeds.entrySet();
     }
 
-//    public static void tellPlayerCurrentRates(World world) {
-//        long seed = world.getSeed();
-//        RNGStreamGenerator dummy = new RNGStreamGenerator();
-//        RNGStreamGenerator main = ((ILevelProperties)world.getLevelProperties()).getRngStreamGenerator();
-//        dummy.blazeRodSeed = main.blazeRodSeed;
-//        dummy.enderEyeSeed = main.enderEyeSeed;
-//        dummy.enderPearlSeed = main.enderPearlSeed;
-//        dummy.featherSeed = main.featherSeed;
-//        dummy.flintSeed = main.flintSeed;
-//        dummy.stringSeed = main.stringSeed;
-//        int total_blazerods = 0;
-//        int total_blazes = 0;
-//        int total_pearls = 0;
-//        int total_endermen = 0;
-//        int broken_eyes = 0;
-//        int total_eyes = 0;
-//        int total_feathers = 0;
-//        int total_chickens = 0;
-//        int total_flint = 0;
-//        int total_gravel = 0;
-//        int total_string = 0;
-//        int total_spiders = 0;
-//        while (total_blazerods < 7) {
-//            int seedResult = (int)dummy.updateAndGetBlazeRodSeed();
-//            boolean didPass = (seedResult % 16 < 8);
-//            if (didPass) {
-//                total_blazerods++;
-//            }
-//            total_blazes++;
-//        }
-//        while (total_pearls < 14) {
-//            int seedResult = (int)dummy.updateAndGetEnderPearlSeed();
-//            boolean didPass = (seedResult % 16 < 10);
-//            if (didPass) {
-//                total_pearls++;
-//            }
-//            total_endermen++;
-//        }
-//        while (total_eyes < 5) {
-//            int seedResult = (int)dummy.updateAndGetEnderEyeSeed();
-//            boolean didPass = (seedResult % 5 > 0);
-//            if (!didPass) {
-//                broken_eyes++;
-//            }
-//            total_eyes++;
-//        }
-//        while (total_feathers < 6) {
-//            int seedResult = (int) dummy.updateAndGetFeatherSeed();
-//            for (int i = 0; i < 2; i++) {
-//                if (total_feathers == 6) {
-//                    break;
-//                }
-//                boolean didPass = (seedResult % 16 < 8);
-//                if (didPass) {
-//                    total_feathers++;
-//                }
-//                seedResult /= 16;
-//            }
-//            total_chickens++;
-//        }
-//        while (total_flint < 6) {
-//            int seedResult = (int) dummy.updateAndGetFlintSeed();
-//            if (seedResult % 10 == 0) {
-//                total_flint++;
-//            }
-//            total_gravel++;
-//        }
-//        while (total_string < 3) {
-//            int seedResult = (int) dummy.updateAndGetStringSeed();
-//            for (int i = 0; i < 2; i++) {
-//                if (total_string == 3) {
-//                    break;
-//                }
-//                boolean didPass = (seedResult % 16 < 8);
-//                if (didPass) {
-//                    total_string++;
-//                }
-//                seedResult /= 16;
-//            }
-//            total_spiders++;
-//        }
-//        ClientPlayerEntity player = MinecraftClient.getInstance().field_3805;
-//        player.addMessage(new TranslatableText(String.format("Current rates on this seed: Blaze rates are %d/%d, Endermen "
-//                        + "rates are %d/%d, eye breaks are %d/%d, feather rates are %d/%d, flint rates are %d/%d, string rates are %d/%d",
-//                total_blazerods, total_blazes, total_pearls, total_endermen, broken_eyes, total_eyes, total_feathers, total_chickens, total_flint, total_gravel,
-//                total_string, total_spiders
-//        )));
-//        world.playSound(player.x, player.y, player.z, "ambient.weather.thunder", 10000.0F, 0.8F + 0.2F);
-//    }
+    /*
+    Unfortunately this code is still ugly because each RNG source does its own independent calculations
+    with the seed.
+     */
+    public static void tellPlayerCurrentRates(World world) {
+        long seed = world.getSeed();
+        RNGStreamGenerator dummy = new RNGStreamGenerator();
+        RNGStreamGenerator main = ((ILevelProperties)world.getLevelProperties()).getRngStreamGenerator();
+        dummy.blazeRodSeed = main.blazeRodSeed;
+        dummy.enderEyeSeed = main.enderEyeSeed;
+        dummy.enderPearlSeed = main.enderPearlSeed;
+        dummy.featherSeed = main.featherSeed;
+        dummy.flintSeed = main.flintSeed;
+        dummy.stringSeed = main.stringSeed;
+        int total_blazerods = 0;
+        int total_blazes = 0;
+        int total_pearls = 0;
+        int total_endermen = 0;
+        int broken_eyes = 0;
+        int total_eyes = 0;
+        int total_feathers = 0;
+        int total_chickens = 0;
+        int total_flint = 0;
+        int total_gravel = 0;
+        int total_string = 0;
+        int total_spiders = 0;
+        while (total_blazerods < 7) {
+            int seedResult = (int)dummy.updateAndGetBlazeRodSeed();
+            boolean didPass = (seedResult % 16 < 8);
+            if (didPass) {
+                total_blazerods++;
+            }
+            total_blazes++;
+        }
+        while (total_pearls < 14) {
+            int seedResult = (int)dummy.updateAndGetEnderPearlSeed();
+            boolean didPass = (seedResult % 16 < 10);
+            if (didPass) {
+                total_pearls++;
+            }
+            total_endermen++;
+        }
+        while (total_eyes < 5) {
+            int seedResult = (int)dummy.updateAndGetEnderEyeSeed();
+            boolean didPass = (seedResult % 5 > 0);
+            if (!didPass) {
+                broken_eyes++;
+            }
+            total_eyes++;
+        }
+        while (total_feathers < 6) {
+            int seedResult = (int) dummy.updateAndGetFeatherSeed();
+            for (int i = 0; i < 2; i++) {
+                if (total_feathers == 6) {
+                    break;
+                }
+                boolean didPass = (seedResult % 16 < 8);
+                if (didPass) {
+                    total_feathers++;
+                }
+                seedResult /= 16;
+            }
+            total_chickens++;
+        }
+        while (total_flint < 6) {
+            int seedResult = (int) dummy.updateAndGetFlintSeed();
+            if (seedResult % 10 == 0) {
+                total_flint++;
+            }
+            total_gravel++;
+        }
+        while (total_string < 3) {
+            int seedResult = (int) dummy.updateAndGetStringSeed();
+            for (int i = 0; i < 2; i++) {
+                if (total_string == 3) {
+                    break;
+                }
+                boolean didPass = (seedResult % 16 < 8);
+                if (didPass) {
+                    total_string++;
+                }
+                seedResult /= 16;
+            }
+            total_spiders++;
+        }
+        ClientPlayerEntity player = MinecraftClient.getInstance().field_3805;
+        player.addMessage(new TranslatableText(String.format("Current rates on this seed: Blaze rates are %d/%d, Endermen "
+                        + "rates are %d/%d, eye breaks are %d/%d, feather rates are %d/%d, flint rates are %d/%d, string rates are %d/%d",
+                total_blazerods, total_blazes, total_pearls, total_endermen, broken_eyes, total_eyes, total_feathers, total_chickens, total_flint, total_gravel,
+                total_string, total_spiders
+        )));
+        world.playSound(player.x, player.y, player.z, "ambient.weather.thunder", 10000.0F, 0.8F + 0.2F);
+    }
 }
---
+++ src/main/java/io/github/marinersfan824/racemod/mixin/rng/CowEntityMixin.java
@@ -0,0 +1,48 @@
+package io.github.marinersfan824.racemod.mixin.rng;
+
+import io.github.marinersfan824.racemod.RNGStreamGenerator;
+import io.github.marinersfan824.racemod.mixinterface.ILevelProperties;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CowEntity.class)
+public abstract class CowEntityMixin extends LivingEntity {
+
+    private RNGStreamGenerator rngStreamGenerator;
+
+    public CowEntityMixin(World world) {
+        super(world);
+    }
+
+    @Inject(method = "dropLoot", at = @At("HEAD"), cancellable = true)
+    private void dropStandardizedLoot(boolean allowDrops, int lootingMultiplier, CallbackInfo ci) {
+
+        int var3 = this.random.nextInt(3) + this.random.nextInt(1 + lootingMultiplier);
+        int var4;
+        for(var4 = 0; var4 < var3; ++var4) {
+            this.dropItem(Items.LEATHER, 1);
+        }
+        World overWorld = ((ServerWorld)this.world).getServer().getWorld();
+        rngStreamGenerator = ((ILevelProperties)overWorld.getLevelProperties()).getRngStreamGenerator();
+        long seedResult = rngStreamGenerator.getAndUpdateSeed("beefSeed");
+        int numDrops = 1 + (int) (seedResult % (3 + lootingMultiplier));
+        for(var4 = 0; var4 < numDrops; ++var4) {
+            if (this.isOnFire()) {
+                this.dropItem(Items.COOKED_BEEF, 1);
+            } else {
+                this.dropItem(Items.BEEF, 1);
+            }
+        }
+        ci.cancel();
+    }
+}

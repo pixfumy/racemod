3710d21bde372ac9ba9d0f86ef1b872f7c72552b
RedLime <redlimerl@gmail.com>
2024-10-16 06:09:57 +0900

    Mob spawn standardization on the tower (y 128+) / Require Fabric Loader 0.15+

--- src/main/java/net/pixfumy/racemod/RNGStreamGenerator.java
+++ src/main/java/net/pixfumy/racemod/RNGStreamGenerator.java
@@ -6,13 +6,13 @@ import java.util.Random;
 import java.util.Set;
 
 public class RNGStreamGenerator {
-    protected HashMap<String, Long> rngSeeds = new HashMap();
+    protected HashMap<String, Long> rngSeeds = new HashMap<>();
 
     public void init(long worldSeed) {
         /*  abstracts some of the standardization into this HashMap. To add a new standardized source of RNG, put your entries
             into this map, use your world's RNGStreamGenerator in a mixin, and then update SeedfinderUtil.tellPlayerCurrentRates if you want to.
         */
-        rngSeeds = new HashMap() {
+        rngSeeds = new HashMap<String, Long>() {
             {
             //  the first 3 seeds use the same XOR salt as sharpie did in the original forge racemod FeelsStrongMan
                 put("blazeRodSeed", worldSeed ^ 64711520272L);
@@ -26,6 +26,7 @@ public class RNGStreamGenerator {
                 put("porkChopSeed", worldSeed ^ 64182641824614L);
                 put("beefSeed", worldSeed ^ 0xFF97FD1823L);
                 put("arrowSeed", worldSeed ^ 0x9A2B3C4D5E6F7A8BL);
+                put("towerSeed", worldSeed ^ 0x5F7E6D8C5E6F7952L);
             }
         };
     }
